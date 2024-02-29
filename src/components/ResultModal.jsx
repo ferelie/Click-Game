9e38d2ad30b36d07f543bbaacbd7f1b58d8d7c99
ferelie <abdallah.yasser0501@gmail.com>
2024-03-01 00:49:39 +0200

    added score calculation

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -6,18 +6,25 @@ const ResultsModal = forwardRef(function ResultsModal(
 ) {
   const userLost = timeRemaining <= 0;
   const formattedTime = (timeRemaining / 1000).toFixed(2);
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
 
   return (
-    <dialog ref={ref} className="result-modal">
+    <dialog ref={ref} className="result-modal" onClose={onReset}>
       <p>
-        {userLost && <h2>You Lost!</h2>}
-        <br />
+        {userLost && <h2>You Lost</h2>}
+        {!userLost && (
+          <h2>
+            You Won! <br /> Your score was <strong>{score}</strong>.
+          </h2>
+        )}
         The target time was <strong>{targetTime}</strong> seconds.
         <br />
         You stopped the timer with <strong>{formattedTime} seconds left</strong>
       </p>
       <form>
-        <button type="submit" onSubmit={onReset} >Play again</button>
+        <button type="submit" onSubmit={onReset}>
+          Play again
+        </button>
       </form>
     </dialog>
   );
